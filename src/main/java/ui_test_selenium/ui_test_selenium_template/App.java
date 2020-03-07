4099eb9319e1796b20ac44ac0ac3ef2584ae2b7e
adityahenanda <henanda.aditya@gmail.com>
2020-03-07 16:14:05 +0700

    setting env
    hardcode chorme drive location

--- src/main/java/ui_test_selenium/ui_test_selenium_template/App.java
+++ src/main/java/ui_test_selenium/ui_test_selenium_template/App.java
@@ -1,13 +1,21 @@
 package ui_test_selenium.ui_test_selenium_template;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\nyo-nyo\\Downloads\\Compressed\\chromedriver_win32_3\\chromedriver.exe");
+    	
+    	//Creating an object of ChromeDriver
+    	WebDriver driver = new ChromeDriver();
+    	driver.manage().window().maximize();
+
+    	driver.get("http://www.google.com");
+    	System.out.println(driver.getTitle());
+
     }
 }
