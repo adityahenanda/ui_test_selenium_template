72a8547a8902d8c0cdb742a2e1c277a1f3a7f8f2
adityahenanda <henanda.aditya@gmail.com>
2020-03-07 13:36:51 +0700

    init
    init

---
+++ src/main/java/ui_test_selenium/ui_test_selenium_template/App.java
@@ -0,0 +1,13 @@
+package ui_test_selenium.ui_test_selenium_template;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
